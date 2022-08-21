3b6fecf20df7bc00275e58e105fca66f62637c33
The Coders Hack <computersciencechamp@gmail.com>
2022-08-21 21:33:10 +0530

    switch

---
+++ Switch.js
@@ -0,0 +1,35 @@
+// const value=10;
+// if(value==10)
+// {
+// console.log("Value is 10 and if case executed");
+// }
+// else{
+// console.log("Else case executed");
+// }
+
+// var fruit="papaya";
+// if(fruit=="Banana")
+// {
+//     console.log("Banana");
+// }
+// else if(fruit=="Apple")
+// {
+//     console.log("Apple");
+// }
+// else
+// {
+//     console.log("Vegetable");
+// }
+
+const person='NonVegetarian';
+switch(person)
+{
+    case 'Vegetarian':
+        console.log("Vegetarian");
+        break;
+    case 'NonVegetarian':
+        console.log("Non Vegetarian");
+        break;
+        default:
+            console.log("Non Vegetarian");
+}
