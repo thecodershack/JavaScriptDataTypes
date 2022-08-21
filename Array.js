fa9a183a6e5a3a0acc2d86c5528d64ccc22a69b7
The Coders Hack <computersciencechamp@gmail.com>
2022-08-21 22:41:12 +0530

    array code

---
+++ Array.js
@@ -0,0 +1,23 @@
+let array1 =new Array(3);
+array1[0]="CUCUMBER";
+array1[1]="Brinjal";
+array1[2]="potato";
+array1.push("tomato");
+console.log(array1);
+array1.pop();
+console.log(array1);
+
+/// Push and pop 
+/// Push for entering values in array
+// Pop for removing values in array
+
+let arr=['a','b','c'];
+console.log(arr);
+
+/// Constant array
+const array2=['ram','sunil','sameer'];
+console.log(array2);
+array2[1] = 'shyam';
+console.log(array2);
+const array3=['RAMAN'];
+array2 = array3;
