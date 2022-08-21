a3a9840fb367577795d5710b5e8fd5a99bd61c37
The Coders Hack <computersciencechamp@gmail.com>
2022-08-21 17:47:45 +0530

    added file

---
+++ var,let,const,difference/index.js
@@ -0,0 +1,50 @@
+// Example 1
+var a =10;
+function f() 
+{
+    console.log(a);
+}
+f();
+console.log(a);
+
+
+// Example 2
+var b=100;
+function f()
+{
+    var b=10;
+    console.log(b);
+}
+f();
+console.log(b);
+
+
+// Example 3
+
+var c = 150;
+var c = 250;
+console.log(c);
+
+// Example 4
+
+console.log(d);
+var d = 299;
+
+
+// Example 5
+
+let e=5;
+//let e=5;
+console.log(e);
+
+
+// Example 6
+
+const h=299;
+function g()
+{
+    h=290;
+    console.log(h);
+}
+g();
+
