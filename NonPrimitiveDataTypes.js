47a3a52bcdb356d07d115ebed7b14f1412854110
The Coders Hack <computersciencechamp@gmail.com>
2022-08-21 16:16:50 +0530

    data types in js

---
+++ NonPrimitiveDataTypes.js
@@ -0,0 +1,20 @@
+let num=[1,2,3,4];
+console.log(num);
+console.log(typeof(num));
+
+
+let object1= {
+    name : 'The Coders Hack',
+    address : 'Bhopal'
+}
+
+console.log(object1);
+console.log(typeof(object1));
+
+function c() 
+{
+console.log("function");
+}
+
+
+console.log(typeof c);
---
+++ PrimitiveDataTypes.js
@@ -0,0 +1,19 @@
+let string1="Hello Coders Hack Channel";
+console.log("Data type "+ typeof(string1));
+console.log(string1);
+
+let number1=100;
+console.log("Data type "+ typeof(number1));
+console.log(number1);
+
+let boolean1=true;
+console.log("Data type "+ typeof(boolean1));
+console.log(boolean1);
+
+let numb=null;
+console.log("Data type "+ typeof(numb));
+console.log(numb);
+
+let n;
+console.log("Data type "+ typeof(n));
+console.log(n);
